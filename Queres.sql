4b0d490af7a7406fd6f7870b0cd4baf9a3d68c47
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-06-13 15:57:47 +0530

    scripts for inserts added

--- Queres.sql
+++ Queres.sql
@@ -12,3 +12,15 @@ CREATE TABLE hobbits(
     hobbitName CHARACTER VARYING(50),
     personality CHARACTER VARYING(50)
 );
+
+INSERT INTO wizards(WizardName, power)
+VALUES
+('Gandalf', 'fireworks'),
+('Sauron', 'rings'),
+('Saruman', 'betrayal');
+
+INSERT INTO elves(elfName, speed)
+VALUES
+('Legolas', 10),
+('Arwen', 9),
+('Elrond', 5);
