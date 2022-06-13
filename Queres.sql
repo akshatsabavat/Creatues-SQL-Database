981c1c4146623748aa28801d799f96d1143c236f
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-06-13 15:55:26 +0530

    scripts for tables added

---
+++ Queres.sql
@@ -0,0 +1,14 @@
+CREATE TABLE wizards(
+    wizardName CHARACTER VARYING(50),
+    power CHARACTER VARYING(50)
+);
+
+CREATE TABLE elves(
+    elfName CHARACTER VARYING(50),
+    speed REAL
+);
+
+CREATE TABLE hobbits(
+    hobbitName CHARACTER VARYING(50),
+    personality CHARACTER VARYING(50)
+);
